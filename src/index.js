7bcd26d2d644dd42f625d7d2c1b91e05b0d97646
Alfred <formacconjoy@gmail.com>
2019-05-24 14:15:03 +0300

    Webpack repair

--- src/index.js
+++ src/index.js
@@ -1,4 +1,15 @@
-import "./style.scss";
-import * as $ from "jquery";
+"use strict"
 
-//$("p").hide();
+import "./style.scss";
+
+let page = document.body;
+let cours = page.getElementsByClassName('shadow');
+
+
+
+for (let i = 0; i < cours.length; i++){
+        cours[i].onclick = function() {
+        this.classList.add('checked');
+        // setTimeout (this.remove(), 1000);
+    }
+}
